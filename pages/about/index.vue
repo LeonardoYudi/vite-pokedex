d348dee8bd15bd32615a451c41d7e1f334adeb47
Your Name <leonardo.iwama25@outlook.com>
2023-11-02 02:14:58 -0400

    feat: renomenado páginas

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -1,13 +1,13 @@
 <template>
     <section class="p-4 flex flex-col gap-3">
         <div class="flex flex-col gap-3">
-            <h1 class="text-4xl font-bold">Pokédex</h1>
+            <h1 class="text-4xl font-medium">About</h1>
             <p class="text-sm">
                 Explore o universo Pokémon e aprimore seus conhecimentos de treinador com a Pokedex.
             </p>
         </div>
         <figure class="gif">
-            <img src="../../assets/icons/pikachu.gif">
+            <img src="../../assets/icons/pikachu.gif" >
         </figure>
     </section>
     
