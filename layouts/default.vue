595c159d24cb4844f4352471afffaf536d20263b
Your Name <leonardo.iwama25@outlook.com>
2023-11-02 01:08:07 -0400

    feat: correção layouts

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,21 +1,31 @@
 <template>
     <div v-if="isMobile" class="h-screen max-h-screen flex flex-col">
-      <main class="w-full h-full p-2 bg-neutral-100">
-        <slot/>
+      <header class="bg-slate-500 shadow-sm rounded-b-md">
+        <div class="flex justify-center items-center h-20">
+            <!-- <SearchBar class="w-full"/> -->
+            <pokeball class="w-10 h-10"/>
+        </div>
+      </header>
+      
+      <main class="max-h-full h-full overflow-y-scroll px-4 py-2">
+        <NuxtPage/>
       </main>
-      <MobileNavBar/>
+     
+      <footer class="w-full">
+        <MobileNavBar class=""/>
+      </footer>
     </div>
-    <div v-else class="h-screen flex">
+    <!-- <div v-else class="h-screen flex">
       <AsideBar/>
       <main class="w-full p-2 bg-neutral-100">
         <slot/>
       </main>
-    </div>
+    </div> -->
 </template>
 <script setup lang="ts">
 import { useStoreBase } from '~/store/useStore';
+import pokeball from '~/assets/icons/pokeball.vue';
 const isMobile = ref()
-
 onMounted(() => {
   handleResize();
   window.addEventListener('resize', handleResize);
